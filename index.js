72d5c2f394dd5594d13c83e494145c2bffe34701
SaintDrew <drew_el12@yahoo.com>
2024-05-05 07:18:12 -0400

    click function

--- index.js
+++ index.js
@@ -50,3 +50,23 @@
 // li.appendChild(deleteBtn); 
 
 
+
+
+var Form1 = document.getElementById("Form1");
+var Form2 = document.getElementById("Form2");
+var Form3 = document.getElementById("Form3");
+
+var Next1 =document.getElementById("Next1");
+var Next2 =document.getElementById("Next2");
+var Back1 =document.getElementById("Back1");
+var Back2 =document.getElementById("Back2");
+
+Next1.onclick = function(){
+    Form1.style.left = "-450px"
+    Form2.style.left = "40px";
+}
+Back1.onclick = function(){
+    Form1.style.left = "40px"
+    Form2.style.left = "450px";
+}
+
