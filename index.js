879a132be8dd5b5c57a2fea82b21fb80d205b16c
SaintDrew <drew_el12@yahoo.com>
2024-05-05 04:29:29 -0400

    Restarting page

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-// TRAVERSING THE DOM // 
-var itemList = document.querySelector('#items');
+// // TRAVERSING THE DOM // 
+// var itemList = document.querySelector('#items');
 
 
-var button = document.getElementById('button')
-var box = document.getElementById('box')
+// var button = document.getElementById('button')
+// var box = document.getElementById('box')
 
-var itemInput = document.querySelector('input[type="text"');
-var form = document.querySelector('form');
-var select = document.querySelector('select'); 
+// var itemInput = document.querySelector('input[type="text"');
+// var form = document.querySelector('form');
+// var select = document.querySelector('select'); 
 
-var from = document.getElementById('addForm')
-var itemList = document.getElementById('items')
-var filter = document.getElementById('filter')
+// var from = document.getElementById('addForm')
+// var itemList = document.getElementById('items')
+// var filter = document.getElementById('filter')
 
-// Form Submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem)
-// Filter event
-// filter.addEventListener('keyup', filterItems)
+// // Form Submit event
+// form.addEventListener('submit', addItem);
+// // Delete event
+// itemList.addEventListener('click', removeItem)
+// // Filter event
+// // filter.addEventListener('keyup', filterItems)
 
-// Add Item
-function addItem(e) {
-    e.preventDefault()
-}
+// // Add Item
+// function addItem(e) {
+//     e.preventDefault()
+// }
 
-// Get Input value 
-var newItem = document.getElementById('item').value;
+// // Get Input value 
+// var newItem = document.getElementById('item').value;
 
-// Create new li element 
-var li = document.createElement('li');
-// Add class 
-li.className = 'list-group-item';
-// Add text node with input value
-li.appendChild(document.createTextNode(newItem));
+// // Create new li element 
+// var li = document.createElement('li');
+// // Add class 
+// li.className = 'list-group-item';
+// // Add text node with input value
+// li.appendChild(document.createTextNode(newItem));
 
-// Create del button element
-var deleteBtn = document.createElement('button');
+// // Create del button element
+// var deleteBtn = document.createElement('button');
 
-// Add classes to del button
-deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-// Append text node 
-deleteBtn.appendChild(document.createTextNode('X'));
+// // Add classes to del button
+// deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+// // Append text node 
+// deleteBtn.appendChild(document.createTextNode('X'));
 
-// Append button to li
-li.appendChild(deleteBtn);
+// // Append button to li
+// li.appendChild(deleteBtn);
 
-// Append li to list 
-li.appendChild(deleteBtn);
+// // Append li to list 
+// li.appendChild(deleteBtn); 
+
+
